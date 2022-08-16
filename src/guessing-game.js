c1269404d047dd0c2a6a20b5b89bb4be4c66a334
Maksim <ilyshev@mail.ru>
2022-08-17 01:37:44 +0300

    add: correct code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -4,19 +4,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+       return this.result = Math.round((this.maxValue + this.minValue) / 2);
+        
     }
-
     lower() {
-
+        return this.maxValue = this.result;
     }
 
     greater() {
-
+        return this.minValue = this.result;
     }
 }
 
